49648e2f314ac11d46252d0dab4e3e47b6bce1ed
Gaëtan <gaetan.truwant@limayrac.fr>
2025-10-14 10:18:15 +0200

    Add files via upload

---
+++ php/sec/sec22/menu.inc.php
@@ -0,0 +1,10 @@
+<?php
+// Menu de l'application
+?>
+<ul>
+  <li>S'<a href="inscription.php">inscrire</a></li>
+  <li>Se <a href="connexion.php">connecter</a></li>
+  <li>Se <a href="deconnexion.php">déconnecter</a></li>
+  <li>Liste des <a href="user_liste.php">utilisateurs</a></li>
+  <li>Liste des <a href="episode_liste.php">épisodes</a></li>
+</ul>
